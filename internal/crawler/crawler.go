6801b41499175dd02a5e0e019655ac2f3e441d26
UnbreakablePotato <swipeytwit@gmail.com>
2026-08-11 11:10:30 +0200

    Added a naive approach to rate limiting so we do not just stop crawling at some point
    Also changed how we handle matchids, visited matchids and puuids

--- internal/crawler/crawler.go
+++ internal/crawler/crawler.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"strings"
 	"sync"
+	"time"
 
 	api "github.com/UnbreakablePotato/CSMServer/internal/API"
 	"github.com/joho/godotenv"
@@ -40,11 +41,14 @@ type Queue struct {
 	VisitedMatches map[string]bool
 	PendingMatches chan string
 	PendingPuuids  chan string
-	mu             *sync.Mutex
+	mu             *sync.RWMutex
 }
 
 var queue Queue
 
+/*
+Gets every challenger players puuid in specified region
+*/
 func InitialRequest() error {
 	fullUrl := "https://euw1.api.riotgames.com/lol/league/v4/challengerleagues/by-queue/RANKED_SOLO_5x5?api_key=" + apiKey
 
@@ -81,98 +85,134 @@ func InitialRequest() error {
 	return nil
 }
 
-func ExtractMatchIds(q *Queue) ([]string, error) {
+func ExtractMatchIds(q *Queue) {
+	for {
+		puuid := <-queue.PendingPuuids
 
-	puuid := <-queue.PendingPuuids
+		fullUrl := "https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/" + puuid + "/ids?start=0&count=5?api_key=" + apiKey
 
-	fullUrl := "https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/" + puuid + "/ids?start=0&count=5?api_key=" + apiKey
-
-	req, err := http.NewRequest("GET", fullUrl, nil)
-	if err != nil {
-		fmt.Printf("Error: %s\n", err)
-		return []string{}, err
-	}
-
-	client := http.Client{}
-
-	res, err := client.Do(req)
-	if err != nil {
-		fmt.Printf("Error: %s\n", err)
-		return []string{}, err
-	}
-
-	data, err := io.ReadAll(res.Body)
-	if err != nil {
-		fmt.Printf("Error: %s\n", err)
-		return []string{}, err
-	}
-
-	interResult := string(data)
-
-	result := strings.Split(interResult, ",")
-
-	for i := range result {
-		result[i] = strings.ReplaceAll(result[i], "\"", "")
-		result[i] = strings.ReplaceAll(result[i], "[", "")
-		result[i] = strings.ReplaceAll(result[i], "]", "")
-		//fmt.Printf("debug: %s\n", result[i])
-
-		q.PendingMatches <- result[i]
-	}
-
-	return result, nil
-}
-
-var intermediateGame api.Game
-
-func ExtractMatchData(matches []string) error {
-
-	for i := range matches {
-		fullUrl := "https://europe.api.riotgames.com/lol/match/v5/matches/" + matches[i] + "?api_key=" + apiKey
 		req, err := http.NewRequest("GET", fullUrl, nil)
 		if err != nil {
 			fmt.Printf("Error: %s\n", err)
-			return err
+
 		}
 
 		client := http.Client{}
 
-		res, err := client.Do(req)
-		if err != nil {
-			fmt.Printf("Error: %s\n", err)
-			return err
+		var res *http.Response
+
+		var httperr error
+		for {
+			res, httperr = client.Do(req)
+			if httperr != nil {
+				//An error here with code 429 means the rate limit has been exceeded
+				//We should retry in 2 min
+				if res.StatusCode == 429 {
+					time.Sleep(120000)
+				}
+				fmt.Printf("Error: %s\n", httperr)
+
+			} else {
+				break
+			}
 		}
 
 		data, err := io.ReadAll(res.Body)
 		if err != nil {
 			fmt.Printf("Error: %s\n", err)
-			return err
+
+		}
+
+		interResult := string(data)
+
+		result := strings.Split(interResult, ",")
+
+		for i := range result {
+			result[i] = strings.ReplaceAll(result[i], "\"", "")
+			result[i] = strings.ReplaceAll(result[i], "[", "")
+			result[i] = strings.ReplaceAll(result[i], "]", "")
+			//fmt.Printf("debug: %s\n", result[i])
+
+			/*
+				If a match has already been added to the queue do not add it again...
+			*/
+			q.mu.Lock()
+			if !q.VisitedMatches[result[i]] {
+				q.PendingMatches <- result[i]
+			} else {
+				q.VisitedMatches[result[i]] = true
+			}
+			q.mu.Unlock()
+		}
+	}
+}
+
+var intermediateGame api.Game
+
+func ExtractMatchData() {
+
+	for {
+		matchID := <-queue.PendingMatches
+		fullUrl := "https://europe.api.riotgames.com/lol/match/v5/matches/" + matchID + "?api_key=" + apiKey
+		req, err := http.NewRequest("GET", fullUrl, nil)
+		if err != nil {
+			fmt.Printf("Error: %s\n", err)
+			//return err
+		}
+
+		client := http.Client{}
+
+		var res *http.Response
+
+		var httperr error
+		for {
+			res, httperr = client.Do(req)
+			if httperr != nil {
+				//An error here with code 429 means the rate limit has been exceeded
+				//We should retry in 2 min
+				if res.StatusCode == 429 {
+					time.Sleep(120000)
+				}
+				fmt.Printf("Error: %s\n", httperr)
+
+			} else {
+				break
+			}
+		}
+
+		data, err := io.ReadAll(res.Body)
+		if err != nil {
+			fmt.Printf("Error: %s\n", err)
+			//return err
 		}
 
 		if err := json.Unmarshal(data, &intermediateGame); err != nil {
 			fmt.Printf("Error: %s\n", err)
-			return err
+			//return err
 		}
+
+		queue.PendingMatches <- intermediateGame.Metadata.MatchID
 	}
 
 	//queue.PendingMatches = append(queue.PendingMatches, intermediateGame.Metadata.MatchID)
-	queue.PendingMatches <- intermediateGame.Metadata.MatchID
-
-	return nil
 }
 
 func AddGameToDB(db *sql.DB) {
 
 }
 
-//goroutine which collects urls
+/*
+	log start time
 
-func CollectUrls() error {
+	if rate limit has not been hit do goroutines
 
-	return nil
-}
+	if rate limit hit wait unti
+*/
 
-func Crawl(url string, db *sql.DB) (bool, error) {
+func Crawl(url string, db *sql.DB) {
 
-	return true, nil
+	go ExtractMatchIds(&queue)
+	go ExtractMatchData()
+	go AddGameToDB(db)
+
 }
